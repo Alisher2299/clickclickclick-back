da272646d5ef7fd806ebed58a19a5dfc7a60dc53
Alisher Zhanayev <zhanayev.a@chocodostavka.kz>
2024-02-20 16:47:41 +0600

    fix: add ValidationError

--- src/clicks/utils.py
+++ src/clicks/utils.py
@@ -1,13 +1,15 @@
 from geopy.geocoders import Nominatim
 
+from rest_framework.exceptions import ValidationError
+
 
 def get_location_info(latitude, longitude):
     geolocator = Nominatim(user_agent="your_geopy_app_name")
-    location = geolocator.reverse((latitude, longitude), language='en')
+    location = geolocator.reverse((latitude, longitude), language="en")
 
-    if location and 'address' in location.raw:
-        country = location.raw['address'].get('country', '')
-        city = location.raw['address'].get('city', None)
+    if location and "address" in location.raw:
+        country = location.raw["address"].get("country", "")
+        city = location.raw["address"].get("city", None)
         return country, city
 
-    return None, None
+    raise ValidationError({"error": "Can't find location"})
